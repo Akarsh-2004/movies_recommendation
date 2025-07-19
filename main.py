d04e7fc9ae5b7c1bb247808538a9fb0ff74c3da4
Akarsh Saklani <117979855+Akarsh-2004@users.noreply.github.com>
2025-07-19 09:15:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Allow CORS for your frontend port (Vite: 5173)
+origins = [
+    "https://moviesideas.netlify.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://moviesideas.netlify.app"],  # <-- your Netlify domain
+    allow_origins=origins,  # or ["*"] for testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/recommend/")
 def recommend(title: str = None, genre: str = None):
